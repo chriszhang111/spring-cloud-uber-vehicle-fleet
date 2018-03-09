05e7b2572d23bab17167070378a17cf382e5890b
chris <869415122@qq.com>
2018-03-09 17:06:12 -0500

    Initial commit

---
+++ fleet-location-service/src/main/java/demo/FaultCode.java
@@ -0,0 +1,29 @@
+package demo;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.HashMap;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Embeddable
+@RequiredArgsConstructor
+public class FaultCode {
+    private String engineMake;
+    private String faultCode;
+    private String faultCodeId;
+    private String faultCodeClassification;
+    private String description;
+    @Column(length=1024)
+    private String repairInstructions;
+    private Long fmi;
+    private String sa;
+    private Long spn;
+
+}
---
+++ fleet-location-simulator/src/main/java/demo/GpsSimulatorApplication.java
@@ -0,0 +1,16 @@
+package demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsSimulatorApplication {
+    public static void main(String[] args){
+
+        SpringApplication.run(GpsSimulatorApplication.class,args);
+    }
+
+
+
+}
