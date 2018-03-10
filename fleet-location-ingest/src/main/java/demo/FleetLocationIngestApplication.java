cbf16ad538a094859001e59ccc1efe77ed7183ed
chris <869415122@qq.com>
2018-03-10 16:23:32 -0500

    update 3 moudles

---
+++ fleet-location-ingest/src/main/java/demo/FleetLocationIngestApplication.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class FleetLocationIngestApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FleetLocationIngestApplication.class);
+    }
+
+}
